02bd5682fdacc432502a2e42bebead13a47a1e6f
Mathias Bynens <mathias@qiwi.be>
2018-08-16 10:31:15 +0200

    Add V8 debug builds as `v8-debug`

---
+++ engines/v8-debug/extract.js
@@ -0,0 +1,67 @@
+// Copyright 2018 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the “License”);
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// <https://apache.org/licenses/LICENSE-2.0>.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an “AS IS” BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+const path = require('path');
+
+const execa = require('execa');
+
+const { Installer } = require('../../shared/installer.js');
+const unzip = require('../../shared/unzip.js');
+
+const extract = ({ filePath, engine, os }) => {
+	return new Promise(async (resolve, reject) => {
+		const tmpPath = path.dirname(filePath);
+		await unzip({
+			from: filePath,
+			to: tmpPath,
+		});
+		const installer = new Installer({
+			engine,
+			path: tmpPath,
+		});
+		installer.installLibrary('icudtl.dat');
+		installer.installLibrary('natives_blob.bin');
+		installer.installLibrary('snapshot_blob.bin');
+		if (os.startsWith('win')) {
+			installer.installBinary(
+				{ 'd8.exe': 'v8-debug.exe' },
+				{ symlink: false }
+			);
+			installer.installScript({
+				name: 'v8-debug.cmd',
+				generateScript: (targetPath) => {
+					return `
+						@echo off
+						"${targetPath}\\v8-debug.exe" --natives_blob="${targetPath}\\natives_blob.bin" --snapshot_blob="${targetPath}\\snapshot_blob.bin" %*
+					`;
+				}
+			});
+		} else {
+			installer.installBinary({ 'd8': 'v8-debug' }, { symlink: false });
+			installer.installScript({
+				name: 'v8-debug',
+				generateScript: (targetPath) => {
+					return `
+						#!/usr/bin/env bash
+						"${targetPath}/v8-debug" --natives_blob="${targetPath}/natives_blob.bin" --snapshot_blob="${targetPath}/snapshot_blob.bin" "$@"
+					`;
+				}
+			});
+		}
+		resolve();
+	});
+};
+
+module.exports = extract;
