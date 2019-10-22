2d64e86cd39e472cc152751ad7352c1ec5d45c21
Larry Ewing <lewing@microsoft.com>
2019-10-22 14:34:42 -0500

    Revert TrimStart

--- shared/installer.js
+++ shared/installer.js
@@ -134,7 +134,7 @@ class Installer {
 			`%~dp0${this.targetRelPath}` :
 			this.targetPath;
 		const contents = generateScript(wrapperPath)
-			.trimStart()
+			.trimLeft()
 			.replace(/\t/g, '');
 		fse.removeSync(to);
 		fse.writeFileSync(to, contents);
